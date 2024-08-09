fe4bf7a3689263c8e448e4ccc6900bd69b995bc4
try4code <sivagmk12@gmail.com>
2024-08-09 11:19:01 -0400

    react book store app

---
+++ react-bookstore/backend/config.js
@@ -0,0 +1,6 @@
+export const PORT = 5555;
+
+export const mongoDBURL = 'mongodb+srv://kecsiva2:KUQUh0VvMOYVMthQ@bookstore.2sbfh.mongodb.net/?retryWrites=true&w=majority&appName=BookStore'
+
+// Please create a free database for yourself.
+// This database will be deleted after tutorial
